9d77d762e6436fb40b2684e68fbdc7aac578fe2a
YUSUKE TANABE <e12707@kmjp.local>
2018-11-27 17:49:56 +0900

    課題に取り組みました

--- main.js
+++ main.js
@@ -34,14 +34,6 @@ console.log('出力：',removeMaxNumberFromArray(numbers));
 console.log('実行後の入力：',numbers);
 
 /**
- * 上記の実装方法だと、今回の課題の意図と異なる気がするので
- * 違う方法でも実装してみました。
- */
-
-
-
-
-/**
  * 課題2: 数値が格納されている配列を引数で受け取り、小さい順に並べ替える
  *   - 関数名は「sortNumbers」とする
  *   - sortNumbers関数は次の1つの引数を持つ
@@ -63,7 +55,7 @@ console.log('実行後の入力：',numbers);
 const sortNumbers = (_numbers) => {
     //戻り値用の配列
     let outputArray = [];
-    //引数の配列の要素数（for文の条件文で使用する）
+    //引数の配列の要素数
     const numbersLength = _numbers.length;
 
     for(let i = 0; i < numbersLength; i++){
@@ -83,13 +75,52 @@ const sortNumbers = (_numbers) => {
     return outputArray;
 }
 
-//実行結果の確認
-const beforeSortNumbers = [1000, 10, 500, 234, 965, 221, 102];
-console.log('入力：',beforeSortNumbers);
-console.log('出力：',sortNumbers(beforeSortNumbers));
-console.log('実行後の入力：',beforeSortNumbers);
-
 /**
  * 上記はsort()を使用するヒントを見ずに実装してしまったので、
  * 以下ではsort()を使用して実装してみました。
  */
+
+const sortNumbers2 = (_numbers) =>{
+    let returnNumbers = _numbers.slice();
+
+    returnNumbers.sort((a, b) => {
+        return a - b;
+    })
+
+    return returnNumbers;
+}
+
+//実行結果の確認
+const beforeSortNumbers = [1000, 10, 500, 234, 965, 221, 102];
+
+console.log('入力：',beforeSortNumbers);
+console.log('出力：',sortNumbers(beforeSortNumbers));
+console.log('実行後の入力：',beforeSortNumbers); 
+
+console.log('入力：',beforeSortNumbers);
+console.log('出力：',sortNumbers2(beforeSortNumbers));
+console.log('実行後の入力：',beforeSortNumbers);
+
+
+/**
+ * 以下は動作確認
+ */
+
+const test1 = (_numbers) =>{
+    _numbers.push(1); 
+}
+
+const test2 = (_numbers) =>{
+    _numbers.push(1); 
+    return _numbers;
+}
+
+const testNumbers = [];
+
+console.log('入力：',testNumbers);
+test1(testNumbers);
+console.log('実行後の入力：',testNumbers); //1が追加される
+
+console.log('入力：',testNumbers);
+console.log('出力：',test2(testNumbers));
+console.log('実行後の入力：',testNumbers); //1が追加される
